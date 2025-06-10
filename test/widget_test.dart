e2f628a4cab8775139304ae7383b05a495a6453b
Yurii Vydai <yuriivydai@gmail.com>
2025-06-10 16:38:30 +0300

    test fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_app_acc_store1/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('App initializes and shows store front', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
+    final prefs = await SharedPreferences.getInstance();
+    
+    await tester.pumpWidget(MyApp(prefs: prefs));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that we're on the store front screen
+    expect(find.text('Store'), findsOneWidget);
+    expect(find.text('Welcome to the Store!'), findsOneWidget);
   });
 }
